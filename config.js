b7d37da49f7474426c537d04b3bf7e69339622cf
OMAR HOSNY <46012679+7osny13@users.noreply.github.com>
2026-01-06 07:30:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
-// Supabase Configuration
-// ⚠️ مهم: استبدل القيم التالية بقيمك الخاصة من Supabase
-
 const SUPABASE_CONFIG = {
-    // ضع هنا Project URL من Supabase
     url: 'https://kjozzklnfsffpdibljpc.supabase.co',
-    
-    // ضع هنا anon public key من Supabase
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtqb3p6a2xuZnNmZnBkaWJsanBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2NjA0MDMsImV4cCI6MjA4MzIzNjQwM30.a5SCZVjDZDpmzbUmQS86xIQWPqIWPTfk0aCT5HyCLkM'
 };
 
